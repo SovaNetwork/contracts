2d3d81f4d4f9368be006f277a51df1c4cb43a788
PowVT <epowlowsky@gmail.com>
2025-02-14 08:28:31 -0500

    rm soldeer, init solady foundry dep

--- src/uBTC.sol
+++ src/uBTC.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
 
-import "@solady/tokens/WETH.sol";
-import "@solady/auth/Ownable.sol";
+import "solady/tokens/WETH.sol";
+import "solady/auth/Ownable.sol";
 
 import "./lib/SovaBitcoin.sol";
 
