2f2edb95b6563535f7456e6e35af51bdf881df7c
PowVT <epowlowsky@gmail.com>
2025-06-01 09:43:47 -0400

    deps

--- src/uBTC.sol
+++ src/uBTC.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
-import "solady/auth/Ownable.sol";
-import "solady/tokens/WETH.sol";
+import "@solady/auth/Ownable.sol";
+import "@solady/tokens/WETH.sol";
 
 import "./lib/SovaBitcoin.sol";
 
