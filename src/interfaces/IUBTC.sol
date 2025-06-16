4cf5400b6857c174b2046bda449b1e85a0a01315
PowVT <epowlowsky@gmail.com>
2025-06-16 11:41:07 -0400

    milo-med-2-p2 (#10)

--- src/interfaces/IUBTC.sol
+++ src/interfaces/IUBTC.sol
@@ -6,10 +6,11 @@ interface IUBTC {
     function depositBTC(uint64 amount, bytes calldata signedTx) external;
     function withdraw(uint64 amount, uint64 btcGasLimit, uint32 btcBlockHeight, string calldata dest) external;
 
-    /// @notice Administrative functions
+    /// @notice Admin functions
     function adminBurn(address wallet, uint256 amount) external;
     function setMinDepositAmount(uint64 _minAmount) external;
     function setMaxDepositAmount(uint64 _maxAmount) external;
+    function setMaxGasLimitAmount(uint64 _maxGasLimitAmount) external;
     function pause() external;
     function unpause() external;
 }
