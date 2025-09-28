97afb2a50a93e0d21bd7d66032d312ebf24e3c2a
PowVT <epowlowsky@gmail.com>
2025-09-28 07:45:00 -0400

    fmt

--- src/SovaBTC.sol
+++ src/SovaBTC.sol
@@ -209,8 +209,9 @@ contract SovaBTC is ISovaBTC, UBTC20, Ownable, ReentrancyGuard {
             revert PendingTransactionExists();
         }
 
-        _pendingUserWithdrawRequests[msg.sender] =
-            UserWithdrawRequest({amount: amount, btcGasLimit: btcGasLimit, operatorFee: operatorFee, destination: dest});
+        _pendingUserWithdrawRequests[msg.sender] = UserWithdrawRequest({
+            amount: amount, btcGasLimit: btcGasLimit, operatorFee: operatorFee, destination: dest
+        });
 
         emit WithdrawSignaled(msg.sender, amount, btcGasLimit, operatorFee, dest);
     }
